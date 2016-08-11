08a8a186472588df2609512415570e3b9d243966
Shoichi Imamura <syoichiimamura@gmail.com>
2016-08-12 00:41:32 +0900

    問題2.2を解答

--- task_9.ml
+++ task_9.ml
@@ -1,6 +1,6 @@
 (* 必須課題の三つは必ず提出する *)
 
-(* 1 逆ポーランド記法 *)
+(* 1.1 逆ポーランド記法 *)
 type stack = int list
 
 let push (n: int) (s: stack): stack =
@@ -49,7 +49,7 @@ let rec run (stk: stack) (str: string) (i: int): stack =
   else stk
 
 (* 処理の最初にスタックに初期値を与え、処理の最後にスタックから答えを取り出す *)
-let eval (str: string): int =
+let eval_1 (str: string): int =
   let stk = run [] str 0 in
   match pop stk with
   (* _が空になるかどうかの確認を追加する *)
@@ -57,15 +57,15 @@ let eval (str: string): int =
                      else failwith "Remaining number error in run"
 
 (* テスト *)
-let test1_1 = eval "123+*4+" = 9;;
+let test1_1 = eval_1 "123+*4+" = 9;;
 (* スタックが途中で不足してしまうのでエラー
 "Empty stack error in pop" *)
-(* let test1_2 = eval "123+*4++";; *)
+(* let test1_2 = eval_1 "123+*4++";; *)
 (* スタックに要素が 2つ以上残ってしまうのでエラー
 "Remaining number error in run" *)
-(* let test1_3 = eval "123+*4";; *)
+(* let test1_3 = eval_1 "123+*4";; *)
 
-(* 2. 論理式の処理 *)
+(* 2.1 論理式の処理 *)
 type formula =
  | Atom of string
  | Not of formula
@@ -82,9 +82,21 @@ let get_atom (fml: formula): string list =
     | Or(f1, f2) -> set_atom f1 (set_atom f2 lst)
   in set_atom fml []
 
-
-
 (* テスト *)
 let test2_1 = get_atom (And(Not(Atom("p")), Or(Atom("q"),Atom("p")))) = ["q"; "p"];;
 
-(* 必修課題は、3.1の代わりに2.3でもいい *)
+
+(* 2.2 論理式の真偽値 *)
+let rec eval_2 (fml: formula) (asn: (string * bool) list): bool =
+  match fml with
+  | Atom(s) -> List.assoc s asn
+  | Not(f1) -> not (eval_2 f1 asn)
+  | And(f1, f2) -> (eval_2 f1 asn) && (eval_2 f2 asn)
+  | Or(f1, f2) -> (eval_2 f1 asn) || (eval_2 f2 asn)
+
+(* テスト *)
+let test2_2_1 = eval_2 (And(Not(Atom("p")),Or(Atom("q"),Atom("p")))) [("p",true);("q",false)] = false;;
+let test2_2_2 = eval_2 (And(Not(Atom("p")),Or(Atom("q"),Atom("p")))) [("p",false);("q",true)] = true;;
+(* let test2_2_3 = eval_2 (And(Not(Atom("p")),Or(Atom("q"),Atom("p")))) [("p", true)];;(* エラーまたはfalse *) *)
+(* let test2_2_4 = eval_2 (And(Not(Atom("p")),Or(Atom("q"),Atom("p")))) [("p", false)];;(* エラー *) *)
+let test2_2_5 = eval_2 (And(Not(Atom("p")),Or(Atom("q"),Atom("p")))) [("p", false); ("q", true); ("r", true)] = true;;
